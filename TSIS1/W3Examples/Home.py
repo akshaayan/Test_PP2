9cb8ff2aa10439b15cc8749cf7fcb6ec3e174cbd
akshaayan <akshaayan@gmail.com>
2025-01-13 11:08:21 +0500

    examples added

--- TSIS1/W3Examples/Home.py
+++ TSIS1/W3Examples/Home.py
@@ -1,1 +1,28 @@
-print("Hello, World!")
+a = "Test string"
+
+#print(a)
+
+a = 12
+
+#print(a)
+
+'''a = True
+b = 'Text'
+c = int('45')
+'''
+#print(a+c)
+
+x = 'text'
+# print(x)
+# print("Hello, World!")
+
+s = 12*3**3+12-100
+
+s1= 3**3*12 ##rate
+s2 = 12-100 ##corr
+s3 = s1+s2 ##rate + corr
+
+print(type(x))
+
+y = input('What is your age?')
+print('Your age is: 25' + y)
